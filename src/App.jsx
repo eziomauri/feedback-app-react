55863eda7efcad826dc648f4fc2a4cc9bd78e6f6
Ezio Mauri <ezioluca.mauri@gmail.com>
2022-09-07 21:19:35 +0200

    removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom' //uses HTML5 history API to keep the users interface in sync with the URL
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
-import Card from './components/shared/Card'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 const App = () => {
